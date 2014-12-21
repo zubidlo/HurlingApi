d4c8db5696ceedd308d054c77816ab6b6207688f
Martin Zuber <zubidlo@gmail.com>
2014-12-21 11:40:59 +0000

    test controller added and cors enabled

--- HurlingApi/App_Start/WebApiConfig.cs
+++ HurlingApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace HurlingApi
 {
@@ -9,6 +11,8 @@ namespace HurlingApi
     {
         public static void Register(HttpConfiguration config)
         {
+            config.EnableCors();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
@@ -18,11 +22,18 @@ namespace HurlingApi
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
-            //config.EnableQuerySupport();
+            config.EnableQuerySupport();
 
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+
+            var json_settings = config.Formatters.JsonFormatter.SerializerSettings;
+
+            json_settings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+            json_settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            json_settings.Formatting = Formatting.Indented;
+
         }
     }
 }
