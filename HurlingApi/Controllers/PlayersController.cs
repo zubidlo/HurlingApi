1a70c4fea1fcdce1a201ba6a91d27956f22b89ad
Martin Zuber <zubidlo@gmail.com>
2015-01-22 20:47:35 +0000

    player controller done

--- HurlingApi/Controllers/PlayersController.cs
+++ HurlingApi/Controllers/PlayersController.cs
@@ -55,5 +55,96 @@ namespace HurlingApi.Controllers
                 return InternalServerError(new Exception("Repository is broken! There is more than one player with Id=" + id + " in the repository."));
             }
         }
+
+        /// <summary></summary>
+        /// <param name="id"></param>
+        /// <param name="playerDTO"></param>
+        /// <returns></returns>
+        [Route("id/{id:int}")]
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> EditPlayer([FromUri] int id, [FromBody] PlayerDTO playerDTO)
+        {
+            if (playerDTO.Id != id)
+            {
+                return BadRequest("Player Id from request URI: " + id + " doesn't match player Id from request body: " + playerDTO.Id + "!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _repository.UpdateAsync(_factory.GeTModel(playerDTO));
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            catch (InvalidOperationException e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        /// <summary></summary>
+        /// <param name="playerDTO"></param>
+        /// <returns></returns>
+        [Route("")]
+        [HttpPost]
+        [ResponseType(typeof(PlayerDTO))]
+        public async Task<IHttpActionResult> PostPlayer([FromBody] PlayerDTO playerDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newPlayer = _factory.GeTModel(playerDTO);
+
+            try
+            {
+                await _repository.InsertAsync(newPlayer);
+                return CreatedAtRoute("DefaultRoute", new { id = playerDTO.Id }, _factory.GetDTO(newPlayer));
+            }
+            catch (InvalidOperationException e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        /// <summary></summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("id/{id:int}")]
+        [HttpDelete]
+        [ResponseType(typeof(PlayerDTO))]
+        public async Task<IHttpActionResult> DeletePlayer([FromUri] int id)
+        {
+            try
+            {
+                var player = await _repository.FindAsync(p => p.Id == id);
+                if (player == null)
+                {
+                    return NotFound();
+                }
+                await _repository.RemoveAsync(player);
+                return Ok(_factory.GetDTO(player));
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest("Deleting player with Id=" + id + " would break referential integrity of the repository. Check the relations between this player and other entities.");
+            }
+        }
+
+        /// <summary></summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
